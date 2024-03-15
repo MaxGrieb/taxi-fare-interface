a84a5e8250f438dceca64c78795008b6dea85d8c
MaxGrieb <157628547+MaxGrieb@users.noreply.github.com>
2024-03-15 11:27:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoibWF4Z3JpZWIiLCJhIjoiY2x0c2lqY2FwMGR6bDJqcXB6NmJ3bHNyZiJ9.reeujn-u8jjoef8l579tAw';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
